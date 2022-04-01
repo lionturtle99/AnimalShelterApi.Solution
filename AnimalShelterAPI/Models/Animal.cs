9d671114768927cfc2d3d1c7b79e454438e4cdb7
Liam Eller <liamthelastson@gmail.com>
2022-04-01 11:59:39 -0700

    Add data annotations to model and confirm full CRUD functionality

--- AnimalShelterAPI/Models/Animal.cs
+++ AnimalShelterAPI/Models/Animal.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelterAPI.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+        [Required]
+        [StringLength(25)]
         public string Name { get; set; }
+        [Required]
         public string Species { get; set; }
+        [Required]
+        [Range(0, 2000, ErrorMessage = "Age must be between 0 and 2000.")]
         public int Age { get; set; }
+        [Required]
         public string Gender { get; set; }
     }
 }
